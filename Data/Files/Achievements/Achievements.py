8f39e8c55e322208208aeb0e9fb0422470999cc1
Kirill <lubister.vip@mail.ru>
2024-05-10 20:02:11 +0300

    first version(updated 1.0.1) commit till version ready: #2 (unplayable)

--- Data/Files/Achievements/Achievements.py
+++ Data/Files/Achievements/Achievements.py
@@ -4,7 +4,11 @@ from tkinter import *
 class Achievements:
     def __init__(self,game):
         self.game=game
-        self.achieve=['N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N','N',]
+        self.achieve=['N','N','N','N','N',
+                      'N','N','N','N','N',
+                      'N','N','N','N','N',
+                      'N','N','N','N','N',
+                      'N',]
         self.texts=[['First. Again','Get 1st counter after Infinity','multi to 1st counter 1.1'],['Twice a week','Get 2nd counter after Infinity','multi to 2nd counter 1.1'],
                     ['3^2=9?','Get 3rd counter after Infinity','multi to 3rd counter 1.1'],['True mathematics','Get 4th counter after Infinity','multi to 4th counter 1.1'],
                     ['Time to BOOST!','Get 5th counter after Infinity','multi to 5th counter 1.1'],['VI','Get 6th counter after Infinity','multi to 6th counter 1.1'],
@@ -15,7 +19,9 @@ class Achievements:
                     ['Timeless Infinity','Buy Infinity without Time accelerators','Time speed * 1.01'],['Boostless Infinity','Buy Infinity without Counter boosts','All counters * 1.01'],
                     ['Infinity Baron','Have 25 Infinities(not IC)','IC gain *2'],['Doom my mood','Unlock Doom','-'],
                     ['Fully upgraded!','Buy all Infinity upgrades','IC gain *2'],['New currency','Start produce Doomed Destruction','-'],
-                    ['Doomed game','Buy 4th Doom Counter','DD gain *1.5'],['Theory of math','Gain 1e8 DD','Unlock first Illusory Aspect'],]
+                    ['Doomed game','Buy 4th Doom Counter','DD gain *1.5'],['Theory of math','Gain 1e8 DD','Unlock first Illusory Aspect'],
+
+                    ['GET RID OUT OF THEORY','Complete 1st Aspect','IC gain *2'],]
         self.images=[PhotoImage(file='Data/Files/images/achievements/1.png'),PhotoImage(file='Data/Files/images/achievements/2.png'),
                      PhotoImage(file='Data/Files/images/achievements/3.png'),PhotoImage(file='Data/Files/images/achievements/4.png'),
                      PhotoImage(file='Data/Files/images/achievements/5.png'),PhotoImage(file='Data/Files/images/achievements/6.png'),
@@ -23,7 +29,9 @@ class Achievements:
                      PhotoImage(file='Data/Files/images/achievements/9.png'),PhotoImage(file='Data/Files/images/achievements/10.png'),
                      PhotoImage(file='Data/Files/images/achievements/11.png'),PhotoImage(file='Data/Files/images/achievements/12.png'),
                      PhotoImage(file='Data/Files/images/achievements/13.png'),PhotoImage(file='Data/Files/images/achievements/14.png'),
-                     PhotoImage(file='Data/Files/images/achievements/15.png')]
+                     PhotoImage(file='Data/Files/images/achievements/15.png'),PhotoImage(file='Data/Files/images/achievements/16.png'),
+                     PhotoImage(file='Data/Files/images/achievements/17.png'),PhotoImage(file='Data/Files/images/achievements/18.png'),
+                     PhotoImage(file='Data/Files/images/achievements/19.png'),PhotoImage(file='Data/Files/images/achievements/20.png'),]
         self.placed=[]
 
     def place(self):
