f562d0ee690054f90ba33671e6bd045fcd8fd1a3
LubYT <100532914+LubYT@users.noreply.github.com>
2024-09-06 22:10:47 +0300

    first version(updated 1.0.1)(playable) commit till version ready: #5

--- Data/Files/Illusory aspects/Illusory aspects.py
+++ Data/Files/Illusory aspects/Illusory aspects.py
@@ -7,11 +7,20 @@ class Illusory_aspects:
         self.game=game
         self.active_1st=False
         self.save=self.game.Save.Aspect_data
-        if self.save=='True':
+        self.available=self.game.Save.Aspect_data
+        if self.available=='True':
             self.game.Menu.add('Illusory')
 
+    def add(self):
+        self.available = 'True'
+        self.game.Menu.add('Illusory')
+
     def place(self):
-        pass
+        self.text=self.game.main_canvas.create_text(self.game.geometry[0]//2,220,text='hi, it is still in work',anchor='center',fill='white',
+                                          justify='center',font=('bahnschrift', 18))
 
     def hide(self):
-        pass
+        self.game.main_canvas.delete(self.text)
+
+    def get_save(self):
+        return self.available+'\n'
